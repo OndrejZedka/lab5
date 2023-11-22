585afb4ed72e467bb4e82778cf510ae7d262a91f
TheoSchlaeppi <81753102+TheoSchlaeppi@users.noreply.github.com>
2023-11-22 14:48:38 +0100

    push

--- vhdl/timer.vhd
+++ vhdl/timer.vhd
@@ -19,6 +19,24 @@ entity timer is
 end timer;
 
 architecture synth of timer is
+    signal sg_cont, sg_to, sg_ito, sg_run,sg_stop,sg_start: std_logic;
+    signal sg_counter, sg_periode : std_logic_vector(31 DOWNTO 0);
+    type state is (counting, notcouting);
+    signal s_state,s_nextsate: state;
 begin
+    counting : Process (clk,n_reset) is
+        if n_reset = '1' then
+            sg_cont <= '0';
+            sg_to <= '0'; 
+            sg_ito <= '0';
+            sg_run <= '0'; 
+            sg_stop <= '0';
+            sg_start <= '0';
+            sg_counter <= (others => '0')
+            sg_periode <= (others => '0')
+        elsif rising_edge(clk) then
+
+        end if;
+    
     -- TEST
 end synth;
