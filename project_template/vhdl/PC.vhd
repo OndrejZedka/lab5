0fd30d57f05b83774aa8be5df9cdc5e65b79288c
Ondrej Zedka <ondrej.zedka@epfl.ch>
2023-12-06 09:29:00 +0100

    complété PC

--- project_template/vhdl/PC.vhd
+++ project_template/vhdl/PC.vhd
@@ -18,7 +18,28 @@ entity PC is
 end PC;
 
 architecture synth of PC is
-    
+    SIGNAL s_address, s_next_address : std_logic_vector(15 downto 0);
 begin
-    
+
+    addr <= X"0000" & s_address;
+
+    flipflop : Process (clk, reset_n) is
+    begin
+        if reset_n = '0' then
+            s_address <= (others => '0');
+        else
+            if rising_edge(clk) then
+                if en = '1' then
+                    s_address <= s_next_address;
+                end if;
+            end if;
+        end if;
+    end process flipflop;
+
+    s_next_address <= 
+            std_logic_vector(unsigned(s_address) + unsigned(imm))   when add_imm = '1'      else
+            imm(13 downto 0) & "00"                                 when sel_imm = '1'      else
+            a(15 downto 2) & "00"                                   when sel_a = '1'        else
+            X"0004"                                                 when sel_ihandler = '1' else
+            std_logic_vector(unsigned(s_address) + 4); 
 end synth;
